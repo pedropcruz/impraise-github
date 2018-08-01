568b59e584c411425691f2c3886976e8dc6b24ba
pedropcruz <pedropcruz@outlook.pt>
2018-08-01 23:26:07 +0100

    add Organization Container

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import MainCSS from './app.css';
+import Organization from '../organization-info/organization';
 
 class App extends Component{
 	render(){
 		return (
-			<MainCSS className="max-width-4 mx-auto my4">
-				Hello World!
+			<MainCSS className="max-width-4 mx-auto my4 px2 py2">
+				<Organization />
 			</MainCSS>
 		);
 	}

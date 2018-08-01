f215c51778c409ce38202b0341432280e6bc7b72
pedropcruz <pedropcruz@outlook.pt>
2018-08-01 20:41:24 +0100

    deleted default files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '../node_modules/basscss/css/basscss.min.css';
+import './global.css';
+import App from './containers/app/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

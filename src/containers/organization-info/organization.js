9f4332d35b0425baa851819c5a65981ba484f87e
pedropcruz <pedropcruz@outlook.pt>
2018-08-02 02:12:38 +0100

    fetch data to oganization component

--- src/containers/organization-info/organization.js
+++ src/containers/organization-info/organization.js
@@ -3,8 +3,42 @@ import Logo from '../../img/logo.png';
 import OrganizationCSS from './organization.css';
 import DescWithIcon from '../../components/desc-with-icon/desc-with-icon';
 
+import axios from 'axios';
+const API = "http://localhost:8000/organization";
+
 class Organization extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			organization: {},
+			isLoading: false,
+			error: null
+		}
+	}
+	componentDidMount(){
+
+		this.setState({
+			isLoading: true
+		});
+		
+		axios.get(API)
+			.then(res => this.setState({ organization: res.data.organization, isLoading: false }))
+			.catch(error => this.setState({error, isLoading: false}))
+	}
+
+
 	render() {
+		const {name, location, websiteUrl, isLoading, error} = this.state.organization;
+
+		if(error){
+			return <p>{error.message}</p>;
+		}
+
+		if(isLoading){
+			return <p>Loading...</p>;
+		}
+
 		return (
 			<OrganizationCSS className="relative mb2">
 				<div className="flex items-center">
@@ -16,9 +50,9 @@ class Organization extends Component {
 						</figure>
 					</div>
 					<div className="col-10 textDetails ml2">
-						<h1 className="h1 mt0 mb2">Impraise</h1>
-							<DescWithIcon icon="location" desc="Amsterdarm/NewYork/Lisbon" classes="inline-block my1"/>
-							<DescWithIcon icon="link" link="https://www.impraise.com" desc="https://www.impraise.com" />
+						<h1 className="h1 mt0 mb2">{name}</h1>
+							<DescWithIcon icon="location" desc={location} classes="inline-block my1"/>
+							<DescWithIcon icon="link" link={websiteUrl} desc={websiteUrl} />
 					</div>
 				</div>
 			</OrganizationCSS>

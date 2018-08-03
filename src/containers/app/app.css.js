7a2789a6e599d25844c00e8bc9ba36749519d712
pedropcruz <pedropcruz@outlook.pt>
2018-08-03 05:09:49 +0100

    fix tag element

--- src/containers/app/app.css.js
+++ src/containers/app/app.css.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 import vars from '../../variables.css';
 
-const MainCSS = styled.div`
+const MainCSS = styled.main`
 	background: ${vars.colors.whiteColor};
-	height: 100vh;
 	box-shadow: 0 4px 6px 4px rgba(0,0,0,.05);
 	border-radius: ${vars.measures.bv0};
 `;

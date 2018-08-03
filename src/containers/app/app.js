d0f34f7db6074c130a31a5764a18e48e46aae911
pedropcruz <pedropcruz@outlook.pt>
2018-08-03 05:09:15 +0100

    fix padding class && added PinnedRepository Component

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import MainCSS from './app.css';
 import Organization from '../organization-info/organization';
+import PinnedRepos from '../pinned-repositories/pinned-repositories';
 
 class App extends Component{
 	render(){
 		return (
-			<MainCSS className="max-width-4 mx-auto my4 px2 py2">
+			<MainCSS className="max-width-4 mx-auto my4 px3 py2">
 				<Organization />
+				<PinnedRepos />
 			</MainCSS>
 		);
 	}

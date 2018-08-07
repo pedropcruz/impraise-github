9d02dd5370b111bf4878f1a4ff974ea309bc78b3
pedropcruz <pedropcruz@outlook.pt>
2018-08-07 01:55:45 +0100

    add new global class

--- src/global.css.js
+++ src/global.css.js
@@ -4,9 +4,16 @@ import vars from './variables.css';
 injectGlobal`
 
 	body{
-		margin: 0;
+		padding: 0;
+		margin:4rem 0;
 		background: ${vars.colors.bgColor};
 		font-family: ${vars.fonts.AppleSystem};
 	}
 
+	.h2{
+		color: ${vars.colors.black};
+		font-size: ${vars.measures.bv4};
+		font-weight: 400;
+		margin: 0 0 ${vars.measures.bv7} 0;
+	}
 `;

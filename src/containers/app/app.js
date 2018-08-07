45fc917987b267e15989a135b85a8748b82bca03
pedropcruz <pedropcruz@outlook.pt>
2018-08-07 01:57:12 +0100

    added new component

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import MainCSS from './app.css';
 import Organization from '../organization-info/organization';
 import PinnedRepos from '../pinned-repositories/pinned-repositories';
+import Repositories from '../repositories/repositories';
 
 class App extends Component{
 	render(){
 		return (
-			<MainCSS className="max-width-4 mx-auto my4 px3 py2">
+			<MainCSS className="max-width-4 mx-auto my4 px3 pt2 pb0">
 				<Organization />
 				<PinnedRepos />
+				<Repositories />
 			</MainCSS>
 		);
 	}
